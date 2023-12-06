ddcd03566a100c980febc3d67fe4a84c241f7311
Sergey Shapochkin <shapochkinsv@mail.ru>
2023-12-06 15:34:35 +0400

    ver 1.8

--- src/main/java/searchengine/repositories/LemmaRepository.java
+++ src/main/java/searchengine/repositories/LemmaRepository.java
@@ -1,11 +1,13 @@
 package searchengine.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import searchengine.model.LemmaModel;
 import searchengine.model.SiteModel;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -14,6 +16,11 @@ public interface LemmaRepository extends JpaRepository<LemmaModel, Integer> {
     LemmaModel findByLemmaAndSite(String lemma, SiteModel siteModel);
 
     Optional<LemmaModel> findBySiteAndLemma(SiteModel site, String name);
+
+    @Query("select a from LemmaModel as a where a.frequency < 300" +
+            " and a.lemma in (:lemmas) " +
+            " and a.site=:site")
+    List<LemmaModel> selectLemmasBySite(Set<String> lemmas, SiteModel site);
 
     Set<LemmaModel> findAllBySite(SiteModel site);
 
--- src/main/java/searchengine/repositories/PageRepository.java
+++ src/main/java/searchengine/repositories/PageRepository.java
@@ -10,6 +10,7 @@ import searchengine.model.SiteModel;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 @Repository
@@ -18,11 +19,15 @@ public interface PageRepository extends JpaRepository<PageModel, Integer> {
     @Query("select COUNT(*) from PageModel AS e group by e.site having e.site =:site")
     Integer findCountBySite(SiteModel site);
 
+    @Query("select p from PageModel as p " +
+            "join IndexModel as i on p.id = i.page.id " +
+            "where i.lemma.lemma =:lemma and p.site =:site")
+    CopyOnWriteArrayList<PageModel> findByLemma(String lemma, SiteModel site);
+
     boolean existsBySiteIdAndPath(Integer siteId, String url);
 
     Optional<PageModel> findBySiteAndPath(SiteModel site, String url);
 
     List<PageModel> findAllByIdIn(List<Integer> id);
-
 
 }
